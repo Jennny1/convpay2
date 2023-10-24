f2c7fa716098f6af101985aba9098bfc7d91dea9
Jennny1 <june9282@gmail.com>
2023-10-24 17:00:13 +0900

    "231024"

--- src/main/java/com/example/convpay/UserClient.java
+++ src/main/java/com/example/convpay/UserClient.java
@@ -8,29 +8,35 @@ import com.example.convpay.dto.PayResponse;
 import com.example.convpay.service.ConveniencePayService;
 import com.example.convpay.type.ConvenienceType;
 import com.example.convpay.type.PayMethodType;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class UserClient {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        // 사용자 -> 편결이 -> 머니
 
-    ApplicationConfig applicationConfig = new ApplicationConfig();
-    // 사용자 -> 편결이 -> 머니
-    ConveniencePayService conveniencePayService = applicationConfig.conveniencePayServiceDiscountPayMethod();
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(ApplicationConfig.class);
 
-    // G25, 결제 1000원
-    PayRequest payRequest = new PayRequest(PayMethodType.CARD, ConvenienceType.G25, 1000);
-    PayResponse payResponse = conveniencePayService.pay(payRequest);
-
-    System.out.println(payResponse);
-
-    // G25, 취소 500원
-    PayCancelRequest payCancelRequest = new PayCancelRequest(PayMethodType.MONEY,
-        ConvenienceType.G25, 500);
-    PayCancelResponse payCancelResponse = conveniencePayService.payCancel(payCancelRequest);
-
-    System.out.println(payCancelResponse);
+        ConveniencePayService conveniencePayService =
+                applicationContext.getBean("conveniencePayService", ConveniencePayService.class);
 
 
-  }
+        // G25, 결제 1000원
+        PayRequest payRequest = new PayRequest(PayMethodType.CARD, ConvenienceType.G25, 1000);
+        PayResponse payResponse = conveniencePayService.pay(payRequest);
+
+        System.out.println(payResponse);
+
+        // G25, 취소 500원
+        PayCancelRequest payCancelRequest = new PayCancelRequest(PayMethodType.MONEY,
+                ConvenienceType.G25, 500);
+        PayCancelResponse payCancelResponse = conveniencePayService.payCancel(payCancelRequest);
+
+        System.out.println(payCancelResponse);
+
+
+    }
 
 }
--- src/main/java/com/example/convpay/config/ApplicationConfig.java
+++ src/main/java/com/example/convpay/config/ApplicationConfig.java
@@ -4,19 +4,33 @@ import com.example.convpay.service.CardAdapter;
 import com.example.convpay.service.ConveniencePayService;
 import com.example.convpay.service.DiscountByPayMethod;
 import com.example.convpay.service.MoneyAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
+@Configuration
 public class ApplicationConfig {
-
-    public ConveniencePayService conveniencePayServiceDiscountConvenience() {
-        return new ConveniencePayService(new HashSet<>(Arrays.asList(new MoneyAdapter(), new CardAdapter())), new DiscountByPayMethod());
-
-    }
-
-    public ConveniencePayService conveniencePayServiceDiscountPayMethod() {
-        return new ConveniencePayService(new HashSet<>(Arrays.asList(new MoneyAdapter(), new CardAdapter())), new DiscountByPayMethod());
+    @Bean
+    public ConveniencePayService conveniencePayService() {
+        return new ConveniencePayService(new HashSet<>(Arrays.asList(moneyAdapter(), getCardAdapter())), getDiscountInterface());
 
     }
+
+    @Bean
+    private static CardAdapter getCardAdapter() {
+        return new CardAdapter();
+    }
+
+    @Bean
+    private static MoneyAdapter moneyAdapter() {
+        return new MoneyAdapter();
+    }
+
+    @Bean
+    public static DiscountByPayMethod getDiscountInterface() {
+        return new DiscountByPayMethod();
+    }
+
 }
